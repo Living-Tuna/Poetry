982a1b81aee73efb1925d110e2be45636f82c1b9
binaryraft <alfu46991@gmail.com>
2026-08-05 12:06:45 +0530

    1.3.4

--- poetry-theme/src/App.jsx
+++ poetry-theme/src/App.jsx
@@ -1,19 +1,9 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
-import { useAuth } from './auth/AuthContext'
-import { useLanguage } from './language/LanguageProvider'
 import PoetryDashboard from './poetry/PoetryDashboard'
-import LoadingScreen from './components/LoadingScreen'
 
 const DEFAULT_LANG = 'en'
 
 export default function App() {
-  const { loading } = useAuth()
-  const { t } = useLanguage()
-
-  if (loading) {
-    return <LoadingScreen text={t('common.loading')} />
-  }
-
   const savedLang = localStorage.getItem('poetry_lang') || DEFAULT_LANG
 
   return (
